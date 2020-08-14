3fd0ff9951b1dadd189a151a4c645f0fc674f0b3
Mike <michael-wilkins@sky.com>
2020-08-14 16:18:31 +0100

    Create new student validation added.

---
+++ KWLeLearning/Migrations/202008141510040_ModifyValidation.cs
@@ -0,0 +1,24 @@
+﻿namespace KWLeLearning.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ModifyValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Students", "Email", c => c.String());
+            AlterColumn("dbo.Students", "Username", c => c.String());
+            AlterColumn("dbo.Students", "Password", c => c.String());
+            AlterColumn("dbo.Students", "Team", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Students", "Team", c => c.String(nullable: false));
+            AlterColumn("dbo.Students", "Password", c => c.String(nullable: false));
+            AlterColumn("dbo.Students", "Username", c => c.String(nullable: false));
+            AlterColumn("dbo.Students", "Email", c => c.String(nullable: false));
+        }
+    }
+}
